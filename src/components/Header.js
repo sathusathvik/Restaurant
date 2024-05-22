29a21907bb4f355bcc4bbd7a06ab040c015d5f27
sathusathvik <sathvikreddyyekollu@gmail.com>
2024-05-22 09:29:17 +0530

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
                     <h2>Little Lemon</h2>
                     <h3>Chicago</h3>
                     <p>We are family owned meditteraneran restaurant, focused on traditional recipes served with a modern twist</p>
-                    <link to ="/booking"><button arial-label="On Click">Reserve Table</button></link>
+                    <link to =""><button arial-label="On Click">Reserve Table</button></link>
                 </div>
             </section>
         </header>
